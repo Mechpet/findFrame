a51e20f60c71a59bef5913da57c4f87604f2da74
Mechpet <blazepet2@gmail.com>
2022-06-05 22:19:14 -0500

    Added frame math

--- findframe.py
+++ findframe.py
@@ -1,43 +1,67 @@
 import cv2
+import ffmpeg
+import numpy as np
 
-videocapture2 = cv2.VideoCapture("DS_ENTERTAINMENT.mp4")
-videocapture1 = cv2.VideoCapture("Opening.mp4")
+sourceCapture = cv2.VideoCapture("DS_ENTERTAINMENT.mp4")
+targetCapture = cv2.VideoCapture("Ending.mp4")
 
 escape = 27
 
-currentFrameIndex = videocapture2.get(cv2.CAP_PROP_POS_FRAMES)
-targetFrameIndex = videocapture1.get(cv2.CAP_PROP_POS_FRAMES)
+sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
+sourceFrameCnt = sourceCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+sourceFPS = sourceCapture.get(cv2.CAP_PROP_FPS)
 
-flag, targetFrame = videocapture1.read()
+sourceStart = sourceFrameCnt * 0.912
+
+targetFrameIndex = targetCapture.get(cv2.CAP_PROP_POS_FRAMES)
+targetFrameCnt = targetCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+targetFPS = targetCapture.get(cv2.CAP_PROP_FPS)
+
+if sourceFPS != targetFPS:
+    print(f"EXCEPTION: Source FPS {sourceFPS} != Target FPS {targetFPS}")
+    quit()
+
+targetFlag, targetFrameImg = targetCapture.read()
 
 frameCnt = 0
 matched = 0
-while videocapture2.isOpened():
-    flag, frame = videocapture2.read()
+maxFrames = -1
+
+sourceCapture.set(cv2.CAP_PROP_POS_FRAMES, sourceStart)
+while sourceCapture.isOpened():
+    sourceFlag, sourceFrameImg = sourceCapture.read()
     
-    if flag:
-        currentFrameIndex = videocapture2.get(cv2.CAP_PROP_POS_FRAMES)
-        print(cv2.absdiff(frame, targetFrame))
-        if cv2.absdiff(frame, targetFrame).all() == 0:
-            print("FOUND SAME FRAME AT ", frameCnt)
-            targetFrameIndex = videocapture1.get(cv2.CAP_PROP_POS_FRAMES)
-            flag, targetFrame = videocapture1.read()
+    if sourceFlag:
+        cv2.imshow("Source capture", sourceFrameImg)
+        print(cv2.absdiff(sourceFrameImg, targetFrameImg).shape)
+        if np.sum(cv2.absdiff(sourceFrameImg, targetFrameImg)) < 1000000:
+            sourceTime = sourceFrameIndex / sourceFPS
+            targetTime = targetFrameIndex / targetFPS
+            print("FOUND SAME FRAME AT FRAME #: ", frameCnt)
+            print("@ source time: ", sourceTime)
+            print("@ target time", targetTime)
+            targetFrameIndex = targetCapture.get(cv2.CAP_PROP_POS_FRAMES)
+            targetFlag, targetFrameImg = targetCapture.read()
             matched += 1
         elif matched:
-            videocapture1.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            flag, targetFrame = videocapture1.read()
+            targetCapture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            targetFlag, targetFrameImg = targetCapture.read()
 
             matched = 0
+        else:
+            print("Diff sum = ", np.sum(cv2.absdiff(sourceFrameImg, targetFrameImg)))
+        
+        sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
     else:
         break
 
     if cv2.waitKey(10) == escape:
         break
-    if frameCnt == 100:
+    if frameCnt == maxFrames:
         break
     frameCnt += 1
 
 print("num matched = ", matched)
-videocapture1.release()
-videocapture2.release()
+sourceCapture.release()
+targetCapture.release()
 cv2.destroyAllWindows()
