b834d2bbdf32880d8400b821cbc843ed4c77691a
Mechpet <blazepet2@gmail.com>
2022-06-06 17:57:19 -0500

    Diff visualization

--- findframe.py
+++ findframe.py
@@ -3,15 +3,17 @@ import ffmpeg
 import numpy as np
 
 sourceCapture = cv2.VideoCapture("DS_ENTERTAINMENT.mp4")
-targetCapture = cv2.VideoCapture("Ending.mp4")
+targetCapture = cv2.VideoCapture("OpeningDefault.mp4")
 
 escape = 27
+space = 32
 
 sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
 sourceFrameCnt = sourceCapture.get(cv2.CAP_PROP_FRAME_COUNT)
 sourceFPS = sourceCapture.get(cv2.CAP_PROP_FPS)
 
-sourceStart = sourceFrameCnt * 0.912
+sourceStart = 3624
+#sourceStart = sourceFrameCnt * 0.912
 
 targetFrameIndex = targetCapture.get(cv2.CAP_PROP_POS_FRAMES)
 targetFrameCnt = targetCapture.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -26,15 +28,21 @@ targetFlag, targetFrameImg = targetCapture.read()
 frameCnt = 0
 matched = 0
 maxFrames = -1
+desiredDim = (400, 225)
+targetFrameImgResized = cv2.resize(targetFrameImg, desiredDim)
 
 sourceCapture.set(cv2.CAP_PROP_POS_FRAMES, sourceStart)
 while sourceCapture.isOpened():
     sourceFlag, sourceFrameImg = sourceCapture.read()
     
     if sourceFlag:
-        cv2.imshow("Source capture", sourceFrameImg)
-        print(cv2.absdiff(sourceFrameImg, targetFrameImg).shape)
-        if np.sum(cv2.absdiff(sourceFrameImg, targetFrameImg)) < 1000000:
+        sourceFrameImgResized = cv2.resize(sourceFrameImg, desiredDim)
+        cv2.imshow("Source capture", sourceFrameImgResized)
+        cv2.imshow("Target capture", targetFrameImgResized)
+        #print(cv2.absdiff(sourceFrameImg, targetFrameImg).shape)
+        diff = cv2.absdiff(sourceFrameImg, targetFrameImg)
+        cv2.imshow("Difference", diff)
+        if not diff.any():
             sourceTime = sourceFrameIndex / sourceFPS
             targetTime = targetFrameIndex / targetFPS
             print("FOUND SAME FRAME AT FRAME #: ", frameCnt)
@@ -42,6 +50,7 @@ while sourceCapture.isOpened():
             print("@ target time", targetTime)
             targetFrameIndex = targetCapture.get(cv2.CAP_PROP_POS_FRAMES)
             targetFlag, targetFrameImg = targetCapture.read()
+            targetFrameImgResized = cv2.resize(targetFrameImg, desiredDim)
             matched += 1
         elif matched:
             targetCapture.set(cv2.CAP_PROP_POS_FRAMES, 0)
@@ -55,8 +64,13 @@ while sourceCapture.isOpened():
     else:
         break
 
-    if cv2.waitKey(10) == escape:
+    key = cv2.waitKey(10)
+    if key == escape:
         break
+    elif key == space:
+        pass
+    else:
+        cv2.waitKey(5000)
     if frameCnt == maxFrames:
         break
     frameCnt += 1
