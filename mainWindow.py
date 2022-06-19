6927742965964cda0cc9c0f8b495787c6c2f85fe
Mechpet <blazepet2@gmail.com>
2022-06-19 15:12:41 -0500

    Added progress bar, interrupt execution

--- mainWindow.py
+++ mainWindow.py
@@ -1,12 +1,14 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QLineEdit, QPushButton, QScrollArea, QTabWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QTabWidget
 from PyQt6.QtCore import Qt, QObject, QThread, pyqtSignal, pyqtSlot
 
 from captureEditor import captureEditor
 from videoSettings import videoSettings
 from outputSettings import outputSettings
-import sliceFunction as slicer
+from progressPreview import progressPreview
+from sliceFunction import sliceWorker, DEFAULT_SLICE_LENGTH, DEFAULT_SSIM
+import config
 
 app = QApplication(sys.argv)
 
@@ -26,12 +28,14 @@ class mainWindow(QWidget):
         self.captureEditor = captureEditor()
         self.videoSettings = videoSettings()
         self.outputSettings = outputSettings()
+        self.progress = progressPreview()
         self.captureEditor.sourceInput.textChanged.connect(self.videoSettings.setAspectRatio)
 
         self.tabWidget = QTabWidget()
         self.tabWidget.addTab(self.captureEditor, "Input settings")
         self.tabWidget.addTab(self.videoSettings, "Slice settings")
         self.tabWidget.addTab(self.outputSettings, "Output settings")
+        self.tabWidget.addTab(self.progress, "Progress")
         self.executeBtn = QPushButton("Execute")
         self.executeBtn.clicked.connect(self.startSlice)
         self.stopBtn = QPushButton("Stop")
@@ -53,7 +57,7 @@ class mainWindow(QWidget):
     @pyqtSlot()
     def stopSlicing(self):
         """Stop the slicing operation of the current worker"""
-        slicer.executingFlag = False
+        config.executingFlag = False
 
     def createSliceWorker(self):
         """Get all of the information the user inputted"""
@@ -64,19 +68,20 @@ class mainWindow(QWidget):
         dimensions = (int(self.videoSettings.compareWidth.text()), int(self.videoSettings.compareHeight.text()))
         for i in range(self.captureEditor.targetList.layout.count() - 1):
             targetFiles.append(self.captureEditor.targetList.layout.itemAt(i).widget().fileEdit.text())
-            targetSSIMs.append(slicer.DEFAULT_SSIM)
+            targetSSIMs.append(DEFAULT_SSIM)
             sourceRanges.append(self.captureEditor.targetList.layout.itemAt(i).widget().bounds.value())
 
         if self.videoSettings.sliceDurationEdit.text():
             sliceDuration = float(self.videoSettings.sliceDurationEdit.text())
         else:
-            sliceDuration = slicer.DEFAULT_SLICE_LENGTH
+            sliceDuration = DEFAULT_SLICE_LENGTH
 
         directory = self.outputSettings.getOutputDirectory()
         template = self.outputSettings.outputTemplate.text()
         prefix = self.outputSettings.appendBtns.checkedId()
 
         self.worker = sliceWorker(sourceFile, targetFiles, targetSSIMs, sourceRanges, sliceDuration, dimensions, directory, template, prefix)
+        self.worker.progressChanged.connect(self.progress.updateValue)
         self.worker.moveToThread(self.thread)
         self.thread.start()
         self.worker.ready.emit()
@@ -87,29 +92,9 @@ class mainWindow(QWidget):
         self.worker = None
         event.accept()
 
-class sliceWorker(QObject):
-    ready = pyqtSignal()
-    def __init__(self, sourceFile, targetFiles, targetSSIMs, sourceRanges, sliceDuration, dimensions, directory, template, prefix):
-        super().__init__()
-        self.sourceFile = sourceFile
-        self.targetFiles = targetFiles
-        self.targetSSIMs = targetSSIMs
-        self.sourceRanges = sourceRanges
-        self.sliceDuration = sliceDuration
-        self.dimensions = dimensions
-        self.directory = directory
-        self.template = template
-        self.prefix = prefix
-        self.ready.connect(self.run)
-
-    @pyqtSlot()
-    def run(self):
-        """Start the slicing operation"""
-        slicer.executingFlag = True
-        slicer.slice(self.sourceFile, self.targetFiles, self.targetSSIMs, self.sourceRanges, self.sliceDuration, self.dimensions, self.directory, self.template, self.prefix)
-
 def main():
-    slicer.executingFlag = False
+    global executingFlag
+    executingFlag = False
     window = mainWindow()
 
     sys.exit(app.exec())
--- sliceFunction.py
+++ sliceFunction.py
@@ -5,112 +5,196 @@ import subprocess
 from skimage.metrics import structural_similarity as ssim
 import numpy as np
 
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
 from timeit import default_timer as timer
 
 from target import target
+import config
 
 showStatus = False
 DEFAULT_SSIM = 0.95
 DEFAULT_SLICE_LENGTH = 60
+
+class sliceWorker(QObject):
+    ready = pyqtSignal()
+    progressChanged = pyqtSignal(float)
+    def __init__(self, sourceFile, targetFiles, targetSSIMs, sourceRanges, sliceDuration, dimensions, directory, template, prefix):
+        super().__init__()
+        self.sourceFile = sourceFile
+        self.targetFiles = targetFiles
+        self.targetSSIMs = targetSSIMs
+        self.sourceRanges = sourceRanges
+        self.sliceDuration = sliceDuration
+        self.dimensions = dimensions
+        self.directory = directory
+        self.template = template
+        self.prefix = prefix
+        self.ready.connect(self.run)
+
+    @pyqtSlot()
+    def run(self):
+        """Start the slicing operation"""
+        config.executingFlag = True
+        self.slice(self.sourceFile, self.targetFiles, self.targetSSIMs, self.sourceRanges, self.sliceDuration, self.dimensions, self.directory, self.template, self.prefix)
+    
+    def match(self, sourceFile, targetFiles, targetSSIMs, sourceRanges, dimensions):
+        sourceCapture = cv2.VideoCapture(sourceFile)
+
+        sourceFrameCnt = sourceCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+        matchStarts = []
+
+        for i in range(len(targetFiles)):
+            matchStarts.append(-1)
+            targetCapture = cv2.VideoCapture(targetFiles[i])
+            print(f"sourceRanges = {sourceRanges}")
+            sourceRange = np.dot(sourceRanges[i], sourceFrameCnt / 100).astype(int)
+
+            print(f"Source range = {sourceRange}")
+            sourceCapture.set(cv2.CAP_PROP_POS_FRAMES, sourceRange[0])
+            sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
+
+            # Get the first frame of the target capture
+            targetFlag, targetFrameImg = targetCapture.read()
+            targetFrameImgResized = cv2.resize(targetFrameImg, dimensions)
+            targetFrameImgResized = cv2.cvtColor(targetFrameImgResized, cv2.COLOR_BGR2GRAY)
+
+            while sourceCapture.isOpened() and config.executingFlag:
+                # Read the next frame of the sourceCapture
+                self.progressChanged.emit((sourceFrameIndex - sourceRange[0]) / sourceRange[1] * 100)
+                sourceFlag, sourceFrameImg = sourceCapture.read()
+    
+                if sourceFlag:
+                    sourceFrameImgResized = cv2.resize(sourceFrameImg, dimensions)
+                    sourceFrameImgResized = cv2.cvtColor(sourceFrameImgResized, cv2.COLOR_BGR2GRAY)
+
+                    # Calculate the Structural Similarity Index between the two grayscale images
+                    ssimFloat = ssim(sourceFrameImgResized, targetFrameImgResized)
+                    print(f"SSIM: {ssimFloat}")
+
+                    if ssimFloat >= targetSSIMs[i]:
+                        if matchStarts[i] < 0:
+                            matchStarts[i] = sourceFrameIndex
+
+                        # Read the next frame of the targetCapture
+                        targetFlag, targetFrameImg = targetCapture.read()
+                        if targetFlag:
+                            targetFrameImg = cv2.cvtColor(targetFrameImg, cv2.COLOR_BGR2GRAY)
+                            targetFrameImgResized = cv2.resize(targetFrameImg, dimensions)
+                        else:
+                            break
+                    elif matchStarts[i]:
+                        targetCapture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                        targetFlag, targetFrameImg = targetCapture.read()
+                        matchStarts[i] = -1
+            
+                    sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
+                # Something failed while trying to read the next frame - safe to end the search
+                else:
+                    break
+
+                # Didn't find a suitable match within the given range
+                if sourceFrameIndex == sourceRange[1] and matchStarts[i] == -1:
+                    break
+
+        return matchStarts
+
+    def slice(self, sourceFile, targetFiles, targetSSIMs, sourceRanges, sliceDuration, dimensions, directory = "", name = "", prefix = False):
+        matchingFrames = self.match(sourceFile, targetFiles, targetSSIMs, sourceRanges, dimensions)
+        print(matchingFrames)
+
+        sourceProps = captureAttributes(sourceFile)
+        targetsProps = [captureAttributes(targetFile) for targetFile in targetFiles]
+        targetMatches = [(matchingFrames[i], matchingFrames[i] + targetsProps[i].frameCnt) for i in range(len(matchingFrames))]
+        # Convert the targetMatches to seconds
+        targetMatches = [(targetMatches[i][0] / targetsProps[i].FPS, targetMatches[i][1] / targetsProps[i].FPS) for i in range(len(targetMatches))]
+
+        sampling = 't'
+        sliceIndex = 1
+        targetIndex = 0
+        if sampling == 'n':
+            start = 0
+            length = int(sourceProps.FPS * sliceDuration)
+            max = sourceProps.frameCnt
+        elif sampling == 't':
+            start = 0
+            length = sliceDuration
+            max = sourceProps.frameCnt / sourceProps.FPS
+        end = length
+        sliceRange = [[start, end]]
+
+        matchStart = targetMatches[targetIndex][0]
+        matchEnd = targetMatches[targetIndex][1]
+        while config.executingFlag:
+            file = open("log.txt", "a")
+            if matchStart >= start and matchStart <= end:
+                file.write("Case 1:\n")
+                sliceRange = [[start, end]]
+                # Found a match within the current slice
+                while matchStart >= sliceRange[-1][0] and matchStart <= sliceRange[-1][1]:
+                    sliceRange[-1][1] = matchStart + 1
+                    sliceRange.append([matchEnd, matchEnd + (length - (matchStart - start))])
+                    targetIndex += 1
+                    if targetIndex < len(targetMatches):
+                        matchStart = targetMatches[targetIndex][0]
+                        matchEnd = targetMatches[targetIndex][1]
+                    else:
+                        break
+
+                betweenStringArgs = betweenString(sliceRange, sampling, max)
+
+                ffmpegCmdSlow = f"C:/ffmpeg/bin/ffmpeg.exe -y -i {sourceFile} "\
+                    f"-filter_complex \"select = "\
+                    f"'{betweenStringArgs}', "\
+                    "setpts = N/FRAME_RATE/TB\" "\
+                    f"-af \"aselect = "\
+                    f"'{betweenStringArgs}', "\
+                    "asetpts = N/SR/TB\" "\
+                    "-map 0 "\
+                    f"{getFileName(directory, name, prefix, sliceIndex)}"
+
+                start = sliceRange[-1][1]
+                if start >= max:
+                    config.executingFlag = False
+            elif end > max:
+                file.write("Case 2:\n")
+                ffmpegCmdSlow = f"C:/ffmpeg/bin/ffmpeg.exe -y -i {sourceFile} "\
+                    "-filter_complex \"select = "\
+                    f"'between({sampling}, {start}, {max})', "\
+                    "setpts = N/FRAME_RATE/TB\" "\
+                    f"-af \"aselect = "\
+                    f"'between({sampling}, {start}, {max})', "\
+                    "asetpts = N/SR/TB\" "\
+                    "-map 0 "\
+                    f"{getFileName(directory, name, prefix, sliceIndex)}"
+                config.executingFlag = False
+            else:
+                file.write("Case 3:\n")
+                ffmpegCmdSlow = f"C:/ffmpeg/bin/ffmpeg.exe -y -i {sourceFile} "\
+                    "-filter_complex \"select = "\
+                    f"'between({sampling}, {start}, {end})', "\
+                    "setpts = N/FRAME_RATE/TB\" "\
+                    f"-af \"aselect = "\
+                    f"'between({sampling}, {start}, {end})', "\
+                    "asetpts = N/SR/TB\" "\
+                    "-map 0 "\
+                    f"{getFileName(directory, name, prefix, sliceIndex)}"
+
+                start = end
+        
+            file.write(ffmpegCmdSlow + "\n")
+            end = start + length
+            file.close()
+            subprocess.call(ffmpegCmdSlow, shell = True)
+
+            sliceIndex += 1
+    
+        print('Finished')
 
 class captureAttributes:
     def __init__(self, fileName):
         capture = cv2.VideoCapture(fileName)
         self.frameCnt = capture.get(cv2.CAP_PROP_FRAME_COUNT)
         self.FPS = capture.get(cv2.CAP_PROP_FPS)
-
-def slice(sourceFile, targetFiles, targetSSIMs, sourceRanges, sliceDuration, dimensions, directory = "", name = "", prefix = False):
-    global executingFlag
-    matchingFrames = match(sourceFile, targetFiles, targetSSIMs, sourceRanges, dimensions)
-    print(matchingFrames)
-
-    sourceProps = captureAttributes(sourceFile)
-    targetsProps = [captureAttributes(targetFile) for targetFile in targetFiles]
-    targetMatches = [(matchingFrames[i], matchingFrames[i] + targetsProps[i].frameCnt) for i in range(len(matchingFrames))]
-    # Convert the targetMatches to seconds
-    targetMatches = [(targetMatches[i][0] / targetsProps[i].FPS, targetMatches[i][1] / targetsProps[i].FPS) for i in range(len(targetMatches))]
-
-    sampling = 't'
-    sliceIndex = 1
-    targetIndex = 0
-    if sampling == 'n':
-        start = 0
-        length = int(sourceProps.FPS * sliceDuration)
-        max = sourceProps.frameCnt
-    elif sampling == 't':
-        start = 0
-        length = sliceDuration
-        max = sourceProps.frameCnt / sourceProps.FPS
-    end = length
-    sliceRange = [[start, end]]
-
-    matchStart = targetMatches[targetIndex][0]
-    matchEnd = targetMatches[targetIndex][1]
-    while executingFlag:
-        file = open("log.txt", "a")
-        if matchStart >= start and matchStart <= end:
-            file.write("Case 1:\n")
-            sliceRange = [[start, end]]
-            # Found a match within the current slice
-            while matchStart >= sliceRange[-1][0] and matchStart <= sliceRange[-1][1]:
-                sliceRange[-1][1] = matchStart + 1
-                sliceRange.append([matchEnd, matchEnd + (length - (matchStart - start))])
-                targetIndex += 1
-                if targetIndex < len(targetMatches):
-                    matchStart = targetMatches[targetIndex][0]
-                    matchEnd = targetMatches[targetIndex][1]
-                else:
-                    break
-
-            betweenStringArgs = betweenString(sliceRange, sampling, max)
-
-            ffmpegCmdSlow = f"C:/ffmpeg/bin/ffmpeg.exe -y -i {sourceFile} "\
-                f"-filter_complex \"select = "\
-                f"'{betweenStringArgs}', "\
-                "setpts = N/FRAME_RATE/TB\" "\
-                f"-af \"aselect = "\
-                f"'{betweenStringArgs}', "\
-                "asetpts = N/SR/TB\" "\
-                "-map 0 "\
-                f"{getFileName(directory, name, prefix, sliceIndex)}"
-
-            start = sliceRange[-1][1]
-            if start >= max:
-                executingFlag = False
-        elif end > max:
-            file.write("Case 2:\n")
-            ffmpegCmdSlow = f"C:/ffmpeg/bin/ffmpeg.exe -y -i {sourceFile} "\
-                "-filter_complex \"select = "\
-                f"'between({sampling}, {start}, {max})', "\
-                "setpts = N/FRAME_RATE/TB\" "\
-                f"-af \"aselect = "\
-                f"'between({sampling}, {start}, {max})', "\
-                "asetpts = N/SR/TB\" "\
-                "-map 0 "\
-                f"{getFileName(directory, name, prefix, sliceIndex)}"
-            executingFlag = False
-        else:
-            file.write("Case 3:\n")
-            ffmpegCmdSlow = f"C:/ffmpeg/bin/ffmpeg.exe -y -i {sourceFile} "\
-                "-filter_complex \"select = "\
-                f"'between({sampling}, {start}, {end})', "\
-                "setpts = N/FRAME_RATE/TB\" "\
-                f"-af \"aselect = "\
-                f"'between({sampling}, {start}, {end})', "\
-                "asetpts = N/SR/TB\" "\
-                "-map 0 "\
-                f"{getFileName(directory, name, prefix, sliceIndex)}"
-
-            start = end
-        
-        file.write(ffmpegCmdSlow + "\n")
-        end = start + length
-        file.close()
-        subprocess.call(ffmpegCmdSlow, shell = True)
-
-        sliceIndex += 1
-    
-    print('Finished')
 
 def betweenString(ranges, sampling, max):
     """Return the string of betweens"""
@@ -123,68 +207,6 @@ def betweenString(ranges, sampling, max):
             strings.append(f"between({sampling}, {range[0]}, {min(max, range[1])})")
     
     return " + ".join(strings)
-
-
-def match(sourceFile, targetFiles, targetSSIMs, sourceRanges, dimensions):
-    global executingFlag
-    sourceCapture = cv2.VideoCapture(sourceFile)
-
-    sourceFrameCnt = sourceCapture.get(cv2.CAP_PROP_FRAME_COUNT)
-    matchStarts = []
-
-    for i in range(len(targetFiles)):
-        matchStarts.append(-1)
-        targetCapture = cv2.VideoCapture(targetFiles[i])
-        print(f"sourceRanges = {sourceRanges}")
-        sourceRange = np.dot(sourceRanges[i], sourceFrameCnt / 100).astype(int)
-
-        print(f"Source range = {sourceRange}")
-        sourceCapture.set(cv2.CAP_PROP_POS_FRAMES, sourceRange[0])
-        sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
-
-        # Get the first frame of the target capture
-        targetFlag, targetFrameImg = targetCapture.read()
-        targetFrameImgResized = cv2.resize(targetFrameImg, dimensions)
-        targetFrameImgResized = cv2.cvtColor(targetFrameImgResized, cv2.COLOR_BGR2GRAY)
-
-        while sourceCapture.isOpened() and executingFlag:
-            # Read the next frame of the sourceCapture
-            sourceFlag, sourceFrameImg = sourceCapture.read()
-    
-            if sourceFlag:
-                sourceFrameImgResized = cv2.resize(sourceFrameImg, dimensions)
-                sourceFrameImgResized = cv2.cvtColor(sourceFrameImgResized, cv2.COLOR_BGR2GRAY)
-
-                # Calculate the Structural Similarity Index between the two grayscale images
-                ssimFloat = ssim(sourceFrameImgResized, targetFrameImgResized)
-                print(f"SSIM: {ssimFloat}")
-
-                if ssimFloat >= targetSSIMs[i]:
-                    if matchStarts[i] < 0:
-                        matchStarts[i] = sourceFrameIndex
-
-                    # Read the next frame of the targetCapture
-                    targetFlag, targetFrameImg = targetCapture.read()
-                    if targetFlag:
-                        targetFrameImg = cv2.cvtColor(targetFrameImg, cv2.COLOR_BGR2GRAY)
-                        targetFrameImgResized = cv2.resize(targetFrameImg, dimensions)
-                    else:
-                        break
-                elif matchStarts[i]:
-                    targetCapture.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                    targetFlag, targetFrameImg = targetCapture.read()
-                    matchStarts[i] = -1
-            
-                sourceFrameIndex = sourceCapture.get(cv2.CAP_PROP_POS_FRAMES)
-            # Something failed while trying to read the next frame - safe to end the search
-            else:
-                break
-
-            # Didn't find a suitable match within the given range
-            if sourceFrameIndex == sourceRange[1] and matchStarts[i] == -1:
-                break
-
-    return matchStarts
 
 def getFileName(directory, name, prefix, index):
     """Get the file name given the template, index, and whether the index is the prefix or suffix"""
